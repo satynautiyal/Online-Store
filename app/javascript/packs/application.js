9407d1e544095e93f26c11ee374a7fb381e1732e
Satish <satynautiyal@gmail.com>
2022-01-08 13:47:21 +0530

    tailwind fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "stylesheets/application" // tailwind
+import "stylesheets/application"
 var $ = require( "jquery" )
 require("slick-carousel")
 
